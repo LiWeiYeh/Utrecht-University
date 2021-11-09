32cafd92536172437cc479a3180721d69d8431ee
Levi <liwei_yeh@hotmail.com>
2021-11-09 17:44:44 +0100

    added exam and exercises as practice

--- Functional-Programming/Functional-Programming/exercises/lec14.hs
+++ Functional-Programming/Functional-Programming/exercises/lec14.hs
@@ -12,3 +12,44 @@ tuple ta tb = do
     a <- ta
     b <- tb
     return (a,b)
+
+get :: State s s
+get = S (id)
+
+modify :: (s -> s) -> State s ()
+modify f = S (\s -> ((), f s))
+
+put :: s -> State s ()
+put s = (\_ -> ((), s))
+
+modify' f = do
+    s <- get
+    put (f s)
+
+put' s = modify (const s)
+
+foldM :: Monad m => (a -> b -> m a) -> a -> [b] -> m a
+foldM f z = foldl'g (return z)
+    where
+        g r x = do
+            a <- r
+            f a x
+
+data Error m a = Error m | OK a
+
+instance Functor (Error m) where
+    fmap f (Error m) = Error m
+    fmap f (Ok a)    = Ok (f a)
+
+
+instance Monad (Error m) where
+    return = Ok
+    (>>=) (Error m) _ = m
+    (>>=) (Ok a) f = f a
+
+
+
+
+
+
+
